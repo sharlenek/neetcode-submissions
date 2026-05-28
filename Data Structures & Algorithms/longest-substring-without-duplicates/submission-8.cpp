9696f3e8b5eb2e5692df012a9e65b481826b4f1c
sharlene <sharlenekho@gmail.com>
2026-05-28 19:55:52 -0400

    Add: longest-substring-without-duplicates - submission-8

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-8.cpp
@@ -0,0 +1,28 @@
+
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int l = 0;
+        int r;
+        int count = 0;
+        int maxcount = 0;
+        unordered_map<char, int> hash;
+        
+        for (int r = 0; r < s.size(); ++r){
+            if (hash.find(s[r]) == hash.end()){
+                hash[s[r]] = r;
+            } else {
+                if (l <= hash[s[r]]){
+                    l = hash[s[r]] + 1;
+                }
+                hash[s[r]] = r;
+            }
+            count = r - l + 1;
+            if (maxcount < count){
+                maxcount = count;
+            }
+        }
+        return maxcount;
+    }
+};
