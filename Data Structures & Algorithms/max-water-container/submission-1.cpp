83e45a826a573e610b9d4b5b73bfba7bf164a872
sharlene <sharlenekho@gmail.com>
2026-07-29 17:33:16 -0400

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxArea(vector<int>& heights) {
+        //need to keep track of the max area
+        // i think its two pointer? not sliding window
+        //i have to decide the condition for moving a side. move the lower one
+
+        int area;
+        int maxarea = 0;
+        int left = 0;
+        int right = heights.size()-1;
+
+        while (left <= right){
+            area = min(heights[left], heights[right])*(right-left);
+            if (area > maxarea){
+                maxarea = area;
+            }
+
+            if (heights[left] <= heights[right]){
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxarea;
+    }
+};
